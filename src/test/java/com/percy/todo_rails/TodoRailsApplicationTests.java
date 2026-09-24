8ec566aab00b3eabaac831fecc34cd3ddbee496e
percent1 <percydc0@gmail.com>
2026-09-24 15:49:40 +0200

    Add repository persistence test

--- src/test/java/com/percy/todo_rails/TodoRailsApplicationTests.java
+++ src/test/java/com/percy/todo_rails/TodoRailsApplicationTests.java
@@ -1,13 +1,32 @@
 package com.percy.todo_rails;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.percy.todo_rails.model.Todo;
+import com.percy.todo_rails.repository.TodoRepository;
 
 @SpringBootTest
 class TodoRailsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TodoRepository todoRepository;
 
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void shouldSaveTodo() {
+        Todo todo = new Todo("Learn automated testing");
+
+        Todo savedTodo = todoRepository.save(todo);
+
+        assertEquals("Learn automated testing", savedTodo.getTitle());
+        assertFalse(savedTodo.isCompleted());
+    }
 }
